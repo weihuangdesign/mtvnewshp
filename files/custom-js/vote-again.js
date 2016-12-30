74d15c6e45dd4d602429bb31f94c5341d186fe80
weihuangdesign <weihuang_design@yahoo.com>
2016-12-30 16:56:29 -0500

    Delete vote-again.js

--- files/custom-js/vote-again.js
+++
@@ -1,7 +0,0 @@
-$('.btn-x').click(function(){
-  $(this).addClass('active');
-});
-
-$('.btn-x').on('animationend webkitAnimationEnd oAnimationEnd', function(){     
-  $('.btn-x').removeClass('active');
-});
